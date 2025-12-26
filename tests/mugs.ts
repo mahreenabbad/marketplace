007124059b8b749df0753a49ef44de55e25442df
Mahreen <mahreenabbad@gmail.com>
2025-12-26 20:38:27 +0800

    first commit

---
+++ tests/mugs.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Mugs } from "../target/types/mugs";
+
+describe("mugs", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.mugs as Program<Mugs>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
